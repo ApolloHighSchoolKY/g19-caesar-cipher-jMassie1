863f5cc8bc7650706136b286d95adb4ec93e3000
jMassie1 <jaden.massie@stu.daviess.kyschools.us>
2025-02-24 14:48:14 +0000

    j

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -9,24 +9,31 @@
 
 public class CaesarCipher {
 
-	private char[][] alphabet;
-	private char[][] shifted;
+	private char[] alphabet;
+	private char[] shifted;
 	private int shift;
 
     public CaesarCipher() {
-
+        alphabet = new char[26];
+        alphabet = 'a' + 'b' + 'c' + 'd' + 'e' + 'f' + 'g' + 'h' + 'i' + 'j' + 'k' + 'l' + 'm' + 'n' + 'o' + 'p' + 'q' + 'r' + 's' + 't' + 'u' + 'v' + 'w' + 'x' + 'y' + 'z';
+        shift = 4;
+        
     }
 
     public CaesarCipher(int num){
-
+        //whatever the shift number is add 4 and shift it 4 positions
+        //What is 22+4%# 
+        shift = num;
     }
 
     public String encrypt(String message){
-
+        //look through alphabet
+        return "" + alphabet[0]; 
     }
 
     public String decrypt(String message){
-
+        //look through shifted
+        return "";
     }
 
     public void shifter(int num){
